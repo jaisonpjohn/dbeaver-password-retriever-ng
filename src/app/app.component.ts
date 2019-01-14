b3364c1b465c747c7ad9d6e87fc2501fbdf5f4e6
JaisonJohn <Jaison.PoikayilJohn@target.com>
2019-01-14 06:08:51 -0600

    beauty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 import { DecryptorService } from "./decryptor.service";
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -9,6 +18,11 @@ import { DecryptorService } from "./decryptor.service";
 })
 export class AppComponent {
   constructor(private decryptorService: DecryptorService) { }
+  emailFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  matcher = new MyErrorStateMatcher();
 
   title = 'DBeaver Password Decrypter!\n';
   encPassword = '';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule, MatInputModule} from '@angular/material';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {DecryptorService} from "./decryptor.service";
 
@@ -19,6 +19,7 @@ import {DecryptorService} from "./decryptor.service";
     MatButtonModule,
     MatInputModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [DecryptorService],
